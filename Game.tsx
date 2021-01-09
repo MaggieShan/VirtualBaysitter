76fefcbec4dd12a55bc442f6f41c588313a40a1e
Amber Liu <aliu294@uwo.ca>
2021-01-09 17:49:18 -0500

    doodler

--- Game.tsx
+++ Game.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { State } from 'react-native-gesture-handler';
-import Doodler from './doodler';
+import Doodler from './Doodler';
 
 export default class Game extends React.Component {
 
@@ -19,13 +19,67 @@ export default class Game extends React.Component {
         doodler: {
             x: 100,
             y: 50
+        },
+        goingLeft: false,
+        goingRight: false,
+    };
+
+    componentDidMount() {
+        setInterval(this.gameTick, 30);
+    }
+
+    gameTick = () => {
+        if (this.state.goingLeft) {
+            this.setState((prevState) => {
+                return {
+                    doodler: {
+                        x: prevState.doodler.x - 5,
+                        y: prevState.doodler.y
+                    }
+                }
+            });
         }
+        if (this.state.goingRight) {
+            this.setState((prevState) => {
+                return {
+                    doodler: {
+                        x: prevState.doodler.x + 5,
+                        y: prevState.doodler.y
+                    }
+                }
+            });
+        }
+    }
+
+    startLeft = () => {
+        this.setState({goingLeft: true})
+    };
+    stopLeft = () => {
+        this.setState({goingLeft: false})
+    };
+    startRight = () => {
+        this.setState({goingRight: true})
+    };
+    stopRight = () => {
+        this.setState({goingRight: false})
     };
 
     render() {
         return (
-            <View style={styles.grid}/>
-            <Doodler x={this.state.doodler.x} y={this.state.doodler.y} />
+            <>
+                <View style={styles.grid}/>
+                <Doodler x={this.state.doodler.x} y={this.state.doodler.y} />
+                <TouchableOpacity
+                    style={styles.leftButton}
+                    onPressIn={this.startLeft}
+                    onPressOut={this.stopLeft}
+                >LEFT</TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.rightButton}
+                    onPressIn={this.startRight}
+                    onPressOut={this.stopRight}
+                >RIGHT</TouchableOpacity>
+            </>
         );
     }
 };
@@ -39,18 +93,10 @@ const styles = StyleSheet.create({
         fontSize: 200,
         textAlign: 'center',
     },
-    
-    doodler: {
-        width: 60,
-        height: 80,
-        backgroundColor: 'rgb(247, 247, 239)',
-        position: 'absolute',
-    },
-    
-    platform: {
-        width: 85,
-        height: 15,
-        backgroundColor: 'green',
-        position: 'absolute',
-    },
+    leftButton: {
+
+    }
+    rightButton: {
+
+    }
   });
